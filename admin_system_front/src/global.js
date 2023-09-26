33457e433fc516245ce37d0df9be0e8643fc77b5
IvanMao714 <maoyifan714@gmail.com>
2023-09-26 21:37:32 +0800

    "Test Maven Github CI"

--- admin_system_front/src/global.js
+++ admin_system_front/src/global.js
@@ -6,7 +6,7 @@
 // const HOST_URL = 'http://sanshicloud.cn:8090/api'
 
 // const HOST_URL = 'http://8.219.62.112:8080'
-const HOST_URL = 'http://127.0.0.1:8080'
+const HOST_URL = 'http://127.0.0.1'
 export default{
     HOST_URL
 }
